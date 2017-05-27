2342cdb7129ec8c4edeeaee0cb99cd41e834039d
1851997v <vishwatejav@gmail.com>
2017-05-27 15:41:45 +0530

    Initial commit

---
+++ Car.java
@@ -0,0 +1,24 @@
+package com.teja;
+
+/**
+ * Created by TEJA on 5/27/2017.
+ */
+public class Car {
+
+private String model;
+private int doors;
+private String color;
+private int windows;
+
+
+    public void setmodel(String model)
+{
+    this.model=model;
+
+}
+public String getmodel()
+{
+    return this.model;
+
+}
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.teja;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car porsche = new Car();
+        Car ferrari =new Car();
+        porsche.setmodel("audi");
+      System.out.println("model is" + porsche.getmodel());
+
+       // porsche.model="audi";
+    }
+}
